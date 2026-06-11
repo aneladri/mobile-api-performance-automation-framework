9d221a4e0f2feed2d7ceb3d0f2520a039f3a66c7
Aneesh Neladri <neladri.aneesh@gmail.com>
2026-06-11 11:48:42 +0530

    Add Log4j2 logging foundation

--- src/test/java/core/base/BaseApiTest.java
+++ src/test/java/core/base/BaseApiTest.java
@@ -1,15 +1,19 @@
 package core.base;
 
 import core.config.ConfigManager;
+import core.utils.LoggerUtil;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseApiTest extends BaseTest {
+
+    private static final Logger logger = LoggerUtil.getLogger(BaseApiTest.class);
 
     protected String baseUrl;
 
     @BeforeMethod
     public void setUpApi() {
         baseUrl = ConfigManager.getRequired("baseUrl");
-        System.out.println("API Base URL: " + baseUrl);
+        logger.info("API Base URL: {}", baseUrl);
     }
 }
--- src/test/java/core/base/BaseTest.java
+++ src/test/java/core/base/BaseTest.java
@@ -1,19 +1,23 @@
 package core.base;
 
 import core.config.ConfigManager;
+import core.utils.LoggerUtil;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
+    private static final Logger logger = LoggerUtil.getLogger(BaseTest.class);
+
     @BeforeSuite
     public void beforeSuite() {
-        System.out.println("===== Test Execution Started =====");
-        System.out.println("Environment: " + ConfigManager.getEnvironment());
+        logger.info("===== Test Execution Started =====");
+        logger.info("Environment: {}", ConfigManager.getEnvironment());
     }
 
     @AfterSuite
     public void afterSuite() {
-        System.out.println("===== Test Execution Completed =====");
+        logger.info("===== Test Execution Completed =====");
     }
 }
